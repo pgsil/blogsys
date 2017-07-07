8490107bbfc16305e2838ea68405e715fc1b945e
pgsil <email@email.com>
2017-07-07 16:40:38 -0300

    fix webedia logo path

--- components/CompHeader.js
+++ components/CompHeader.js
@@ -37,7 +37,7 @@ export default class CompHeader extends Component {
 						</div>
 
 						<div className="level-item">
-							<img src="/images/webedia.png" alt="Um Site do Webedia Group" />
+							<img src="/static/images/webedia.png" alt="Um Site do Webedia Group" />
 						</div>
 					</div>
 				</div>
